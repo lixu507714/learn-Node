5290eaf6718037a5280e2d74cebd574b644ac23b
lixu <lx507714@163.com>
2017-12-17 11:04:46 +0800

    app.js

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,9 +2,11 @@ console.log('hello world.');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./nodes.js');
 
 // fs.appendFile('greetings.txt', 'Hello World');
 var user = os.userInfo(); //  所在的文件夹
-console.log(user);
-fs.appendFire('greetings.txt', `Hello ${user.username}!`);
+console.log(notes);
+console.log('Result:', notes.add(1, 2));
+fs.appendFile('greetings.txt', `Hello ${user.username}! you are ${notes.addNodes}`);
 
---
+++ notes-node/nodes.js
@@ -0,0 +1,8 @@
+console.log('Starting notes.js');
+module.exports.age = 22;
+module.exports.addNodes = () => {
+  return  'New note';
+};
+module.exports.add = (a, b) => {
+    return a+b;
+};
