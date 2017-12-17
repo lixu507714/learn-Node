f71a3f53aec0a7ab9a132657d7045ed3738273de
lixu <lx507714@163.com>
2017-12-17 11:06:58 +0800

    update

--- notes-node/app.js
+++ notes-node/app.js
@@ -8,5 +8,6 @@ const notes = require('./nodes.js');
 var user = os.userInfo(); //  所在的文件夹
 console.log(notes);
 console.log('Result:', notes.add(1, 2));
+
 fs.appendFile('greetings.txt', `Hello ${user.username}! you are ${notes.addNodes}`);
 
