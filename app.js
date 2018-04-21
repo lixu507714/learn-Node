d6c48c28903fc7009649ead439986e71b24e78a7
lixu <lx507714@163.com>
2018-04-21 18:54:14 +0800

    know

--- app.js
+++ app.js
@@ -50,7 +50,6 @@ const argv = yargs
 // console.log(json);
 var command = argv._[0];
 // console.log(_.isString(true));
-// console.log(_.isString('Andrew'));
 
 // fs.appendFile('greetings.txt', 'Hello World');
 // var user = os.userInfo(); //  所在的文件夹
