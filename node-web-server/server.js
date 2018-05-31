311e4a6feb4656a7067654ca1c28f73859e99e1c
lixu <lx507714@163.com>
2018-05-31 17:22:40 +0800

    knowledge

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs'); // Handlebars.js 模板引擎
 const fs = require('fs');
 
+// const port = process.env.PORT || 3000;
 var app = express();
 // nodemon server.js -e js,hbs  运行
 hbs.registerPartials(__dirname + '/views/partials');
